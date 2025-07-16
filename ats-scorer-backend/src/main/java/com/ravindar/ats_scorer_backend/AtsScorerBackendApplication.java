1d9fa03064589472d63544ac84367395d991d743
ravindar <ravindardeeti799@gmail.com>
2025-07-16 22:37:05 +0530

    init commit

---
+++ ats-scorer-backend/src/main/java/com/ravindar/ats_scorer_backend/AtsScorerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ravindar.ats_scorer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsScorerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsScorerBackendApplication.class, args);
+	}
+
+}
---
+++ ats-scorer-backend/src/test/java/com/ravindar/ats_scorer_backend/AtsScorerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravindar.ats_scorer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsScorerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
