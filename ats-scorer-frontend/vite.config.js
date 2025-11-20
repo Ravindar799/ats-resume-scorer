b0691c66ee9057901af530241d5eb9393ffc40d5
ravindar <ravindardeeti799@gmail.com>
2025-11-20 12:40:06 +0530

    vite error solved

--- ats-scorer-frontend/vite.config.js
+++ ats-scorer-frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // 👇 Add your Render domain here
+    allowedHosts: ["ats-resume-scorer-2.onrender.com"],
+    host: true,
+    port: 4173
+  }
 })
